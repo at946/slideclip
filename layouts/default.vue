e5065e524eccf695446fbf31a400d371cac43e1f
Asato TAKAHASHI <asato730@gmail.com>
2021-01-28 17:44:04 +0900

    Footerを最下部に固定

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <nav class="header">
+  <div class="body">
+    <header class="header">
       <span class="title">SlideClip</span>
-    </nav>
+    </header>
 
-    <nuxt />
+    <main>
+      <nuxt />
+    </main>
+
+    <footer class="footer py-4">
+    </footer>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+
 .header {
   padding: 1rem;
   border-bottom: solid 1px lightgray;
@@ -16,5 +31,9 @@
   .title {
     font-weight: bold;
   }
+}
+
+.footer {
+  background-color: gray;
 }
 </style>
